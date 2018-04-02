4182f6e7aecf3e43b67095f8fe5e65af58528d19
nguyen van dai <vandai.dev.bkhcmut@gmail.com>
2018-04-02 12:31:29 +0700

    Initial Commit

---
+++ SecurityNetWork/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SecurityNetWork
+//
+//  Created by dai nguyen on 4/2/18.
+//  Copyright © 2018 dai nguyen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SecurityNetWork/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SecurityNetWork
+//
+//  Created by dai nguyen on 4/2/18.
+//  Copyright © 2018 dai nguyen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
